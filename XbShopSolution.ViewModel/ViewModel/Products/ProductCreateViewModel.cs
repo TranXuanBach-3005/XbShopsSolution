bc1ebceae11ef8d9ffa80d5f8ac8aca270d0f75e
BACH-PC\XuanBach <bachtran3005@gmail.com>
2023-03-11 11:35:34 +0700

    upcode view model

---
+++ XbShopSolution.ViewModel/ViewModel/Products/ProductCreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XbShopSolution.Data.Enums;
+
+namespace XbShopSolution.ViewModel.ViewModel.Products
+{
+    public class ProductCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Description { set; get; }
+        public string Details { set; get; }
+        public decimal Price { get; set; }
+        public decimal OriginaPrice { get; set; }
+        public int Stock { get; set; }
+        public bool? IsFeatured { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string SeoDescription { set; get; }
+        public string SeoTitle { set; get; }
+        public string SeoAlias { set; get; }
+        public string SeoKeyword { get; set; }
+        public string MetaTitle { get; set; }
+        public Status Status { get; set; }
+        public IFormFile ProductslImage { get; set; }
+    }
+}
---
+++ XbShopSolution.ViewModel/ViewModel/Products/ProductViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XbShopSolution.ViewModel.ViewModel.Products
+{
+    public class ProductViewModel
+    {
+        public int Id { set; get; }
+        public decimal Price { set; get; }
+        public decimal OriginaPrice { set; get; }
+        public int Stock { set; get; }
+        public int ViewCount { set; get; }
+        public DateTime DateCreated { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Details { set; get; }
+        public string SeoDescription { set; get; }
+        public string SeoTitle { set; get; }
+        public string SeoAlias { set; get; }
+        public string SeoKeyword { get; set; }
+        public string MetaTitle { get; set; }
+        public bool? IsFeatured { get; set; }
+        public string ProductslImage { get; set; }
+        public List<string> Categories { get; set; } = new List<string>();
+    }
+}
